84f6ce88021e13097ada033b61258aad571ee62d
Student <student@EPBYMINW2476.minsk.epam.com>
2019-02-25 12:13:25 +0300

    HOMEWORK2_Andrei_Charviakou

---
+++ task2/task5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        if num <= 2 ** 32 and num > 0:
+            n = (bin(num)[2:])
+            n = n.replace("0", "u")
+            n = n.replace("1", "0")
+            n = n.replace("u", "1")
+            c = int(n, 2)
+            num = c
+            print(num)
+        else:
+            print("The wrong input")
+        return num
+
+        
+num = int(input())
+res = Solution()
+res.findComplement(num)
