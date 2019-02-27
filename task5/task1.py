3ea2f880c15959ab669d9dc8c9ed56bb3252988a
Student <student@EPBYMINW2476.minsk.epam.com>
2019-02-27 17:46:53 +0300

    HOMEWORK5_Andrei_Charviakou

---
+++ task5/task1.py
@@ -0,0 +1,7 @@
+import random
+
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+year =  random.randint(1, 2200)    #int(input())
+print(is_leap(year))
---
+++ task5/test1.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from task1 import is_leap
+
+
+class TestTask1(TestCase):
+
+  def setUp(self):
+    """Init"""
+  def test_is_leap(self):
+    """Test for is _prime"""
+    self.assertFalse(is_leap(1900))
+    self.assertTrue(is_leap(2000))
+ 
+  def tearDown(self):
+    """Finish"""
