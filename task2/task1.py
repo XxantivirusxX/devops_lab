c7995bc89f78692e32bf868832546393ab6104ba
Student <student@EPBYMINW2476.minsk.epam.com>
2019-02-27 13:51:44 +0300

    HOMEWORK2_Andrei_Charviakou

--- task2/task1.py
+++ task2/task1.py
@@ -1,15 +1,4 @@
 def is_leap(year):
-    if(year % 4) == 0:
-        if(year % 100) == 0:
-            if(year % 400) == 0:
-                return False
-            else:
-                return True
-        else:
-            return False
-    else:
-        return False
-
-
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 year = int(input())
 print(is_leap(year))
